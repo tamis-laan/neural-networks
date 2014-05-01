c1f665da94cc2db8dfb39d1e6506ee121a7f67e3
Your Name <you@example.com>
2014-05-01 12:26:54 +0200

    visualize joints as movie

---
+++ visualize_raw_joints_movie.m
@@ -0,0 +1,12 @@
+function [] = visualize_raw_joints_movie(joints)
+    s = size(joints);
+    s = s(1);
+
+    for i = 1:1:s
+        visualize_raw_joints(joints,i);
+        pause(0.01);
+        clf;
+    end
+
+end
+
