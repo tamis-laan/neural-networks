9653e60ac30515b1eb505362ee016e55e5b28e97
Frescote <totalvarez@gmail.com>
2014-05-01 11:05:17 +0200

    Joint extraction function

---
+++ extract_joints.m
@@ -0,0 +1,27 @@
+function [joints]=extract_joints(filename) 
+[~,~,data]=xlsread(filename);
+
+%% Joint Extraction
+
+joints=cell(1490,1);
+frame=0;
+
+for i=1:size(data,1)
+%     for j=1:size(data,2)
+        if strcmp(data{i,1}, 'Tracked Skeleton Frame: ')== 1
+            frame = frame+1;
+            joints{frame}=zeros(20,3);
+            for k=1:20
+                for l=1:3
+                   joints{frame}(k,l)=data{i+2*k,l+4};
+                end
+            end
+        end
+        
+%     end
+end
+        
+
+%%
+
+end
