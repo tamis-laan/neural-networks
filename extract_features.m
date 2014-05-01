c174a2731295a4ceb15aad0035786e5fc2356270
Frescote <totalvarez@gmail.com>
2014-05-01 12:41:01 +0200

    Extract Features function
    Low level features defined. However, the angles ones need to be
    normalized to an axis ( probably the hip axis is the best)

--- extract_features.m
+++ extract_features.m
@@ -28,12 +28,33 @@ for i=1:size(joints,1)
     features{i}(11)=abs(joints{i}(5,3)-joints{i}(9,3))/(joints{i}(4,2)-joints{i}(20,2));
     
     % Shoulder orientation
-    
+    features{i}(12)=(joints{i}(9,1)-joints{i}(3,1))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(13)=(joints{i}(9,2)-joints{i}(3,2))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(14)=(joints{i}(9,2)-joints{i}(3,3))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(15)=(joints{i}(5,1)-joints{i}(3,1))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(16)=(joints{i}(5,2)-joints{i}(3,2))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(17)=(joints{i}(5,3)-joints{i}(3,3))/(joints{i}(4,2)-joints{i}(20,2));
     
     % Feet distance
+    features{i}(18)=abs(joints{i}(20,1)-joints{i}(16,1))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(19)=abs(joints{i}(20,2)-joints{i}(16,2))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(20)=abs(joints{i}(20,3)-joints{i}(16,3))/(joints{i}(4,2)-joints{i}(20,2));
     
     % Feet orientation
+    features{i}(21)=(joints{i}(20,1)-joints{i}(19,1))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(22)=(joints{i}(20,2)-joints{i}(19,2))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(23)=(joints{i}(20,2)-joints{i}(19,3))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(24)=(joints{i}(16,1)-joints{i}(15,1))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(25)=(joints{i}(16,2)-joints{i}(15,2))/(joints{i}(4,2)-joints{i}(20,2));
+    features{i}(26)=(joints{i}(16,3)-joints{i}(15,3))/(joints{i}(4,2)-joints{i}(20,2));
     
+    %Head angles
+    a=joints{i}(4,:)-joints{i}(3,:);
+    b=joints{i}(2,:)-joints{i}(3,:);
+    features{i}(27)=atan2(norm(cross(a,b)), dot(a,b));
+    a=joints{i}(4,:)-joints{i}(3,:);
+    b=joints{i}(9,:)-joints{i}(3,:);
+    features{i}(28)=atan2(norm(cross(a,b)), dot(a,b));
     
 end
 end
