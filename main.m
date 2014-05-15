9096c66d8095205ce92c3a501f2fb14c492513cf
Your Name <you@example.com>
2014-05-15 13:10:28 +0200

    Minor changes

--- main.m
+++ main.m
@@ -1,4 +1,5 @@
-joints = extract_joints('example_file.txt')
+joints = extract_joints('example_file.txt');
 features =  extract_group_features_sitting(joints,ones(1782,1));
 windowed_features = window_features(features,3);
-%elm_train(features,0,10,'sig')
+elm_train(windowed_features,0,50,'sig')
+elm_predict(windowed_features(10,2:end))
