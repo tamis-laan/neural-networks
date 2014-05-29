cdd2b5aece0f637ffbcf26221da05b684823e332
António <totalvarez@gmail.com>
2014-05-29 16:30:56 +0200

    No spam patch

--- extract_group_features_sitting.m
+++ extract_group_features_sitting.m
@@ -19,29 +19,29 @@ function out = extract_group_feature_sitting(joints,label,frame)
     HandRight       = joints{frame}(12,:);
     %% Extract Features
                         t   = Head-ShoulderCenter;
-    shoulders_head          = t/norm(t)
+    shoulders_head          = t/norm(t);
                         t   = ShoulderLeft-ShoulderCenter;
-    shoulders_shoulderleft  = t/norm(t)
+    shoulders_shoulderleft  = t/norm(t);
                         t   = ShoulderRight-ShoulderCenter;
-    shoulders_shoulderright = t/norm(t)
+    shoulders_shoulderright = t/norm(t);
     
-    body_angle              = acos( ShoulderCenter/norm(ShoulderCenter) * [0,1,0]' )/(2*pi)
+    body_angle              = acos( ShoulderCenter/norm(ShoulderCenter) * [0,1,0]' )/(pi);
     
                         t1  = (ShoulderLeft-ElbowLeft)/norm(ShoulderLeft-ElbowLeft);
                         t2  = (WristLeft-ElbowLeft)/norm(WristLeft-ElbowLeft);
-    elbow_angle_left        = acos(t1*t2')/(2*pi)
+    elbow_angle_left        = acos(t1*t2')/(pi);
     
                         t1  = (ElbowLeft-WristLeft)/norm(ElbowLeft-WristLeft);
                         t2  = (HandLeft-WristLeft)/norm(HandLeft-WristLeft);
-    hand_angle_left        = acos(t1*t2')/(2*pi)
+    hand_angle_left        = acos(t1*t2')/(pi);
     
                         t1  = (ShoulderRight-ElbowRight)/norm(ShoulderRight-ElbowRight);
                         t2  = (WristRight-ElbowRight)/norm(WristRight-ElbowRight);
-    elbow_angle_right       = acos(t1*t2')/(2*pi)
+    elbow_angle_right       = acos(t1*t2')/(pi);
     
                         t1  = (ElbowRight-WristRight)/norm(ElbowRight-WristRight);
                         t2  = (HandRight-WristRight)/norm(HandRight-WristRight);
-    hand_angle_right        = acos(t1*t2')/(2*pi)
+    hand_angle_right        = acos(t1*t2')/(pi);
     out = [label,shoulders_head,shoulders_shoulderleft,shoulders_shoulderright,body_angle,elbow_angle_left,hand_angle_left,elbow_angle_right,hand_angle_right];
 
 end
